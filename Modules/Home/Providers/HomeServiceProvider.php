2df8ac86b6a7719abff5f6608218c2745105f822
Hetal Nathvani <khushbu.dadhania@cygnetinfotech.com>
2019-12-11 16:48:36 +0530

    Laravel Module Package Demo

---
+++ Modules/Home/Providers/HomeServiceProvider.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Modules\Home\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class HomeServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+        $this->registerFactories();
+        $this->loadMigrationsFrom(module_path('Home', 'Database/Migrations'));
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            module_path('Home', 'Config/config.php') => config_path('home.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            module_path('Home', 'Config/config.php'), 'home'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = resource_path('views/modules/home');
+
+        $sourcePath = module_path('Home', 'Resources/views');
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ],'views');
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/home';
+        }, \Config::get('view.paths')), [$sourcePath]), 'home');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = resource_path('lang/modules/home');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'home');
+        } else {
+            $this->loadTranslationsFrom(module_path('Home', 'Resources/lang'), 'home');
+        }
+    }
+
+    /**
+     * Register an additional directory of factories.
+     *
+     * @return void
+     */
+    public function registerFactories()
+    {
+        if (! app()->environment('production') && $this->app->runningInConsole()) {
+            app(Factory::class)->load(module_path('Home', 'Database/factories'));
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
