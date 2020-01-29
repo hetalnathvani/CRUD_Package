ff6b8416d2369c9aebf3c62010c9fbfec6f4ee14
Hetal Nathvani <khushbu.dadhania@cygnetinfotech.com>
2020-01-29 13:11:40 +0530

    Adding Unmerged Vue files

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h1>Layout</h1>
+        <p>Welcome to my first Inertia.js app!!!</p>
+        <inertia-link href="/about">About </inertia-link>
+        <inertia-link href="/contact">Contact </inertia-link>
+        <inertia-link href="/welcome">Welcome </inertia-link>
+    </header>
+</template>
+    
+<script>
+
+import { InertiaLink } from "@inertiajs/inertia-vue";
+
+export default {
+    props: {
+      title: String,
+    },
+}
+</script>
